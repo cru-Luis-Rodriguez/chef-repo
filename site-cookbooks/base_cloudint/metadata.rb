a8538f9d1fab6ef0e0c032884e70a9fe4a73d954
neillturner <neillwturner@gmail.com>
2014-03-12 12:27:45 +0000

    updates to pass foodcritic

---
+++ site-cookbooks/base_cloudint/metadata.rb
@@ -0,0 +1,9 @@
+ name             'base_cloudinit'
+ description      'Deploys and configures cloud_init script'
+ long_description '# base chef cookbook\n\nDeploys and configures base cloud_init script\n'
+ maintainer_email 'neillwturner@gmail.com'
+ license          'Apache 2.0'
+ version          '1.0.0'
+ supports         'debian', '>= 0.0.0'
+ supports         'ubuntu', '>= 0.0.0'
+  
---
+++ site-cookbooks/mycompany_webserver/metadata.rb
@@ -0,0 +1,9 @@
+name              'mycompany_webserver'
+description       'Deploys and configures webserver'
+long_description  '# mycompany_webserver chef cookbook\n\nDeploys and configures webserver\n'
+maintainer_email  'neillwturner@gmail.com'
+license           'Apache 2.0'
+version           '1.0.0'
+supports          'debian',  '>= 0.0.0',
+supports          'ubuntu',  '>= 0.0.0'
+depends           'apache2', '>= 0.0.60'
