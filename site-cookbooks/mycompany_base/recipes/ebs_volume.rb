bbe1da7ac9d04150f2eedca0048c753a71d15c7a
luis rodriguez <luis.rodriguez@cru.org>
2014-11-21 15:59:23 -0500

    test change to fix FC001

--- site-cookbooks/mycompany_base/recipes/ebs_volume.rb
+++ site-cookbooks/mycompany_base/recipes/ebs_volume.rb
@@ -1,6 +1,6 @@
 aws = data_bag_item("aws", "main")
 
-aws_ebs_volume node[:base][:volume] do
+aws_ebs_volume node['base']['volume'] do
       aws_access_key aws['aws_access_key_id']
       aws_secret_access_key aws['aws_secret_access_key']
       size 50
