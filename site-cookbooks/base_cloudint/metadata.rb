bbb30f5a143ec2f2bbe66c81869869cbfa69be27
luis rodriguez <luis.rodriguez@cru.org>
2014-11-21 16:21:16 -0500

    fix fc007

--- site-cookbooks/base_cloudint/metadata.rb
+++ site-cookbooks/base_cloudint/metadata.rb
@@ -6,6 +6,4 @@ license          'All rights reserved'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "base::centos"
-depends "base::ubuntu"
-depends "base::windows"
+depends "base"
