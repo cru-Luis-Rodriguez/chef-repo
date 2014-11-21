6a6db2e58c6dbd0af4f6b4a2f781483d097fefa2
luis rodriguez <luis.rodriguez@cru.org>
2014-11-21 16:19:28 -0500

    modified the dependency

--- site-cookbooks/base_cloudint/metadata.rb
+++ site-cookbooks/base_cloudint/metadata.rb
@@ -5,3 +5,7 @@ maintainer_email 'infrastructure.team@mycompany.com'
 license          'All rights reserved'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "base::centos"
+depends "base::ubuntu"
+depends "base::windows"
